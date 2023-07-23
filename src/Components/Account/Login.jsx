3d7d25b825a38f3e8cc39a16b075194309839c34
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-07-23 10:28:07 +0100

    new change

--- src/Components/Account/Login.jsx
+++ src/Components/Account/Login.jsx
@@ -16,7 +16,7 @@ export const Login = () => {
                 })
             }
         }} className="py-[100px] z-[70] bg-Tp h-full fixed flex justify-center items-center top-0 w-full px-[40px] ">
-                  <form action=""  className="flex flex-col  bg-white font-poppins rounded p-[40px] relative gap-5">
+                  <form action=""  className="flex flex-col items-center justify-center  bg-white font-poppins rounded p-[40px] relative gap-5">
                   <p onClick={() => {
                     setAccount({
                         login: false,
@@ -24,9 +24,9 @@ export const Login = () => {
                     })
                   }} className="absolute right-[20px] p top-1 bg-slate-900 px-[15px] py-[5px] hover:bg-red-500 cursor-pointer rounded-full text-white font-bold text-[20px] ">x</p>
                     <h1 className="text-center text-[20px] font-semibold ">Welcome back!</h1>
-                    <input className="outline-0  border font-[300] text-[20px] p-2 rounded " type="text" placeholder="email address" name="email" id="" />
-                  <input className="outline-0  border font-[300]  text-[20px] p-2 rounded " type="password" placeholder="password" name="" id="" />
-                  <button className="bg-slate-900 hover:bg-slate-600 text-slate-50 text-[20px] p-2 rounded ">Login</button>
+                    <input className="outline-0  w-full border font-[300] text-[20px] p-2 rounded " type="text" placeholder="email address" name="email" id="" />
+                  <input className="outline-0 w-full border font-[300]  text-[20px] p-2 rounded " type="password" placeholder="password" name="" id="" />
+                  <button className="bg-slate-900 hover:bg-slate-600 text-slate-50 text-[20px] w-full p-2 rounded ">Login</button>
                   <p>Don't have an account yet? <Link className="font-bold underline" onClick={() => {
                     setAccount({
                         login: false,
--- src/Components/Account/Signup.jsx
+++ src/Components/Account/Signup.jsx
@@ -17,7 +17,7 @@ export const SignUp = () => {
             }
         }} className="py-[100px] flex-row z-[70] bg-Tp h-full fixed flex justify-center items-center top-0 w-full px-[40px] ">
           
-                <form action=""  className="flex flex-col  bg-white font-poppins rounded p-[40px] relative gap-5">
+                <form action=""  className="flex flex-col items-center bg-white font-poppins rounded p-[40px] relative gap-5">
                     <h1 className="text-center text-[20px] font-semibold ">Create Account!</h1>
                     <p onClick={() => {
                     setAccount({
@@ -26,12 +26,12 @@ export const SignUp = () => {
                     })
                   }} className="absolute right-[5px] p top-[3px] bg-slate-900 px-[15px] py-[5px] hover:bg-red-500 cursor-pointer rounded-full text-white font-bold text-[20px] ">x</p>
                     <div className="flex md:flex-row gap-2 flex-col">
-                   <input type="text"placeholder="Firstname" className="outline-0  text-[20px] font-[300]  bg-transparent border p-2 rounded "  name="" id="" />
-                   <input type="text" placeholder="Lastname" className="outline-0  text-[20px] font-[300]  bg-transparent border p-2 rounded "  name="" id="" />
+                   <input type="text" placeholder="Firstname" className="outline-0 w-full text-[20px] font-[300]  bg-transparent border p-2 rounded "  name="" id="" />
+                   <input type="text" placeholder="Lastname" className="outline-0 w-full   text-[20px] font-[300]  bg-transparent border p-2 rounded "  name="" id="" />
                     </div>
                     <input className="outline-0 font-[300] border bg-transparent  text-[20px] p-2 rounded " type="email" placeholder="email address" name="email" id="" />
-                  <input className="outline-0 font-[300]  bg-transparent border text-[20px] p-2 rounded " placeholder="Pasword" type="password" name="" id="" />
-                  <button  className="bg-slate-900 hover:bg-slate-600 text-slate-50 text-[20px] p-2 rounded ">Signup</button>
+                  <input className="outline-0 w-full font-[300]  bg-transparent border text-[20px] p-2 rounded " placeholder="Pasword" type="password" name="" id="" />
+                  <button  className="bg-slate-900 hover:bg-slate-600 text-slate-50 text-[20px] p-2 w-full rounded ">Signup</button>
                   <p className="text-center">Already have an account? <Link className="font-bold underline" onClick={() => {
                     setAccount({
                         login: true,
