2a0face10a1d73549dd319a16161c68f55a16ce1
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-08-12 16:25:29 +0100

    new change

--- src/Components/Overview/Overview1.jsx
+++ src/Components/Overview/Overview1.jsx
@@ -10,18 +10,18 @@ export const Overview1 = () => {
 
   const theOverView = fetchedNews[0];
     return(
-        <Link key={theOverView?.id} to={`/blog/${theOverView?.newsHeadline}`} onClick={() => Subscribe(theOverView)}  className=" items-center flex-row flex py-[50px] min-h-[100vh] ">
-<div className="flex flex-col md:flex-row justify-around items-center gap-5">
+        <Link key={theOverView?.id} to={`/blog/${theOverView?.newsHeadline}`} onClick={() => Subscribe(theOverView)}  className=" items-center my-[100px] flex-row flex py-[50px]  ">
+<div className="flex flex-col md:flex-row justify-around items-center gap-y-[40px] gap-x-[70px]">
 <div data-aos='fade-up' data-aos-duration='1000' className="  flex flex-col gap-5 ">
-<h1 className="text-slate-900 capitalize md:hidden md:text-[45px] leading-[45px] md:leading-[50px] text-[40px]   font-poppins">{theOverView?.newsHeadline}</h1>
-        <img src={theOverView?.newsImg} alt="" className="rounded" srcSet="" />
+<h1 className="text-slate-900 capitalize md:hidden md:text-[30px] leading-[45px] md:leading-[50px] text-[25px] font-bold  font-poppins">{theOverView?.newsHeadline}</h1>
+        <img src={theOverView?.newsImg} alt="" className="rounded md:max-w-[500px]" srcSet="" />
        </div>
   <div data-aos='fade-up' data-aos-duration='1000' className="flex-start flex flex-col gap-4">
-    <h1 className="text-slate-900 max-w-[600px] hidden md:flex md:text-[45px] leading-[45px] md:leading-[50px] text-[40px] capitalize  font-poppins">{theOverView?.newsHeadline}</h1>
-    <div className="text-slate-300 font-fonty max-w-[700px] flex flex-col gap-2 ">
-    <p className="md:text-[20px] text-slate-800 text-[18px] font-poppins  ">{!theOverView?.newsOverview ? 'Be your best self, alone and with others, and cultivate deep and lasting friendships and relationships.' : theOverView?.newsOverview } </p>
+    <h1 className="text-slate-900 max-w-[800px] hidden md:flex md:text-[25px] leading-[45px] md:leading-[50px] font-bold text-[25px] font-poppins capitalize  ">{theOverView?.newsHeadline}</h1>
+    <div className="text-slate-300 font-fonty max-w-[800px] flex flex-col gap-2 ">
+    <p className="md:text-[17px] text-slate-800 text-[12px] font-poppins  ">{!theOverView?.newsOverview ? 'Be your best self, alone and with others, and cultivate deep and lasting friendships and relationships.' : theOverView?.newsOverview } </p>
   </div>
-  <Link to={`/blog/${theOverView?.newsHeadline}`} className="text-start bg-slate-900 text-slate-50 text-[30px] rounded-[1px] hover:bg-slate-800 w-fit p-3">Read more...</Link>
+  <Link to={`/blog/${theOverView?.newsHeadline}`} className="text-start bg-slate-900 text-slate-50 text-[18px] rounded-[1px] hover:bg-slate-800 w-fit p-2">Read more...</Link>
   </div>
 </div>
         </Link>
--- src/Components/Overview/Overview2.jsx
+++ src/Components/Overview/Overview2.jsx
@@ -12,16 +12,16 @@ export const Overview2 = () => {
 
     return(
         <Link key={theOverView?.id} to={`/blog/${theOverView?.newsHeadline}`} onClick={() => Subscribe(theOverView)} className="  py-[50px] min-h-[100vh] ">
-<div className="flex flex-col font-poppins md:flex-row-reverse justify-around items-center gap-5">
+<div className="flex flex-col font-poppins md:flex-row-reverse justify-around items-center gap-x-[70px]">
 <div data-aos='fade-up' data-aos-duration='1000' className="  flex flex-col gap-5 ">
-        <img src={!theOverView?.newsImg ? HeroImg : theOverView?.newsImg } alt="" className="rounded" srcSet="" />
+        <img src={!theOverView?.newsImg ? HeroImg : theOverView?.newsImg } alt="" className="rounded md:max-w-[500px]" srcSet="" />
        </div>
   <div data-aos='fade-up' data-aos-duration='1000' className="flex-start flex flex-col gap-4">
-    <h1 className="text-slate-900 max-w-[600px]  md:text-[45px] leading-[45px] md:leading-[50px] text-[40px] capitalize  ">{theOverView?.newsHeadline}</h1>
+    <h1 className="text-slate-900 capitalize md:text-[30px] leading-[45px] md:leading-[50px] text-[25px] font-bold  font-poppins">{theOverView?.newsHeadline}</h1>
     <div className="text-slate-300 font-fonty max-w-[700px] flex flex-col gap-2 ">
-    <p className="md:text-[20px] font-poppins text-slate-800 text-[18px] ">{!theOverView?.newsOverview ? 'Be your best self, alone and with others, and cultivate deep and lasting friendships and relationships.' : theOverView?.newsOverview } </p>
+    <p className="md:text-[20px] font-poppins text-slate-800 text-[13px] ">{!theOverView?.newsOverview ? 'Be your best self, alone and with others, and cultivate deep and lasting friendships and relationships.' : theOverView?.newsOverview } </p>
   </div>
-  <Link to={`/blog/${theOverView?.newsHeadline}`} className="text-start bg-slate-900 text-slate-50 text-[30px] rounded-[1px] hover:bg-slate-800 w-fit p-3">Read more...</Link>
+  <Link to={`/blog/${theOverView?.newsHeadline}`} className="text-start bg-slate-900 text-slate-50 text-[12px] rounded-[1px] hover:bg-slate-800 w-fit p-3">Read more...</Link>
   </div>
 </div>
         </Link>
